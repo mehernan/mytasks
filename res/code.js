1dd7ef5eb4483251518a09cfeb7a4ca50e6f1931
Hernan <hernandocarmo@gmail.com>
2025-03-31 09:33:15 -0300

    first commit

---
+++ res/code.js
@@ -0,0 +1,20 @@
+var lista_tarefas = {
+  "0":{
+    "chackbox":false,
+    "description": "Make app"
+  }
+};
+
+function addtask(txt) {
+  if (text != null) {
+    text.toStrig();
+    // TODO: Criar Chackbox e Salvar no Objeto Json
+    // TODO: Create Chackbox and Save on a Json Object
+  }
+}
+function seetask(param) {
+  // Show tasks
+}
+function toggletaskchack(ntask) {
+  // TODO: Toggle chackbox
+}
